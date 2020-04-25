e66012f61fd411145d6d7d83cec8b74e4c9073d9
ShadowMoose <theshadowmoose@gmail.com>
2020-04-24 17:40:33 -0700

    Enhanced ignore.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const { badgen } = require('badgen');
-const { isMatch } = require('micromatch');
 const fs = require('fs').promises;
 const path = require('path');
 const core = require('@actions/core');
+const { glob } = require('glob-gitignore');
 
 
 const st = Date.now();
@@ -20,19 +20,6 @@ for (const en of Object.keys(process.env)) {
 }
 
 if (debug) core.info('Debugging enabled.');
-
-async function isDir (path) {
-	const stats = await fs.stat(path);
-	return stats.isDirectory();
-}
-
-function norm(base, file) {
-	let r = path.resolve(file).replace(/\\/g, '/');
-	return r
-		.replace(path.resolve(base)
-			.replace(/\\/g, '/'), '')
-		.replace(/^\//, '');
-}
 
 
 async function countLines(fullPath) {
@@ -60,35 +47,26 @@ const countThrottled = throttle(countLines, 10);
  * @param dir {string} The path to check.
  * @param patterns {string[]} array of patterns to match against.
  * @param negative {string[]} array of patterns to NOT match against.
- * @param oBase {string|null} Leave as null, used internally for recursion.
  * @return {Promise<{ignored: number, lines: number, counted: number}>} An array of all files located, as absolute paths.
  */
-async function getFiles (dir, patterns = [], negative = [], oBase=null) {
-	const subDirs = await fs.readdir(dir);
-	const base = oBase || dir;
+async function getFiles (dir, patterns = [], negative = []) {
 	let lines = 0, ignored=0, counted=0;
-	await Promise.all(subDirs.map(async (subdir) => {
-		const fullPath = path.resolve(dir, subdir);
-		if (await isDir(fullPath)) {
-			const res = await getFiles(fullPath, patterns, negative, base);
-			lines += res.lines;
-			ignored += res.ignored;
-			counted += res.counted;
-		} else {
-			const np = norm(base, fullPath);
-			if ((!patterns.length || isMatch(np, patterns)) && !isMatch(np, negative)) {
-				if (debug) core.info(`Counting:${np}`);
-				try {
-					lines += await countThrottled(fullPath);
-					counted++;
-				} catch (err) {
-					core.error(err);
-				}
-			} else {
-				ignored++;
+
+	await glob(patterns, {
+		cwd: dir,
+		ignore: negative,
+		nodir: true
+	}).then(files => {
+		return Promise.all(files.map( async f => {
+			try {
+				if (debug) core.info(`Counting: ${f}`);
+				lines += await countThrottled(f);
+				counted ++;
+			} catch (err) {
+				core.error(err);
 			}
-		}
-	}));
+		}))
+	});
 
 	return { lines, ignored, counted };
 }
